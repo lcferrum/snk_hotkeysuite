5853146871383f3d13450c8a3553d20fabbf1515
lcferrum <lcferrum@yandex.com>
2017-03-04 02:12:50 +0400

    A lot of explanation on creating notification area icon and startup process of Explorer

--- TaskbarNotificationAreaIcon.cpp
+++ TaskbarNotificationAreaIcon.cpp
@@ -71,10 +71,23 @@ TskbrNtfAreaIcon::TskbrNtfAreaIcon(HINSTANCE hInstance, UINT icon_wm, const wcha
 	wcsncpy(icon_ntfdata.szTip, icon_tooltip, 63);	//First version of NOTIFYICONDATA only allowes szTip no more than 64 characters in length (including NULL-terminator)
 	
 	//We are not checking Shell_NotifyIcon result for a reason
-	//If Shell_NotifyIcon will be called before Explorer has been fully initialized - it will fail
+	//If Shell_NotifyIcon will be called before Explorer has been fully initialized (actually it's taskbar thread that handles notification area) - it will fail
 	//This may happen if app is launched with Autorun or Task Scheduler
-	//After explorer has been fully initialized - it will send WM_TASKBARCREATED to all top-level windows (added in shell32.dll v4.71)
+	//Internally Shell_NotifyIcon uses SendMessageTimeout w/ SMTO_ABORTIFHUNG flag and 4 sec (7 sec for Vista+) timeout to check if Explorer is ready 
+	//After Explorer (it's taskbar, which is in separate thread) has been fully initialized - it will send WM_TASKBARCREATED to all top-level windows (added in shell32.dll v4.71 aka Win2k+/98+)
+	//On NT4 Shell_NotifyIcon uses plain SendMessage instead of SendMessageTimeout which is synchronous - it will wait (possibly indefinitely) for Explorer to respond
+	//So at least long Explorer initialization time is not of concern for NT4, but restarted Explorer is a problem here because WM_TASKBARCREATED is missing and no one will tell app to recreate icon
 	//Oldschool way of keeping notification area icon visible in case of Explorer is not ready or crashed is spamming (like every several seconds) NIM_MODIFY and calling NIM_ADD after NIM_MODIFY failed
+	
+	//Like said previously, taskbar of Explorer lives on a separate thread and has a window of "Shell_TrayWnd" class
+	//At the very beginning Explorer does some initializing, including setting up special folders and running HKLM\RunOnceEx and HKLM\RunOnce apps and then launches taskbar thread
+	//First thing that taskbar thread does is creates it's "Shell_TrayWnd" windows, then builds Start Menu and, before launching message loop, broadcasts WM_TASKBARCREATED (if on Win2k+)
+	//Only after message loops is fired, it receives message (sent by taskbar itself) to run every other startup app: HKLM\Run, HKCU\Run, HKCU\RunOnce, subkeys of HKLM\RunOnce, HKCU\RunOnce, HKLM\Run and HKCU\Run
+	//All those startups are done in separate thread so as not to block message loop
+	//And ofcourse all the startups are done only if Explorer was launched as part of logon process and not restarted
+	//So Autorun apps are actually run after taskbar has been created and done most of it's initialization stuff and, more importantly, taskbar thread shoudn't be blocked during this time
+	//But apps from Task Scheduler are run at unspecified moment because services (including Task Scheduler itself) are launched before Explorer
+	//So apps launched by task may try to create notification area icon even before taskbar's "Shell_TrayWnd" window is created
 	Shell_NotifyIcon(NIM_ADD, &icon_ntfdata);
 	
 	/*if (!Shell_NotifyIcon(NIM_ADD, &icon_ntfdata)) {

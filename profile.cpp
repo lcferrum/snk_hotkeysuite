13808b08da4fe586cd54dbf46d1eec69efdc2728
lcferrum <lcferrum@yandex.com>
2017-03-16 00:36:33 +0400

    Working x86 KeyTriplet2

--- profile.cpp
+++ profile.cpp
@@ -35,7 +35,7 @@ private:
 	EventHandlerFn hk_event_handler;
 public:
 	KeyTriplet2(wchar_t* cmdline_s, wchar_t* cmdline_l): 
-		hk_sc(DEFAULT_SC), hk_ext(DEFAULT_EXT), hk_state(0), hk_engaged(0), hk_down_tick(0), hk_cmdline_s(cmdline_s), hk_cmdline_l(hk_cmdline_l), hk_si{sizeof(STARTUPINFO)}, hk_event_handler(SinglePressCtrlAltEventHandler) {}
+		hk_sc(DEFAULT_SC), hk_ext(DEFAULT_EXT), hk_state(0), hk_engaged(0), hk_down_tick(0), hk_cmdline_s(cmdline_s), hk_cmdline_l(cmdline_l), hk_si{sizeof(STARTUPINFO)}, hk_event_handler(SinglePressCtrlAltEventHandler) {}
 	void ResetEventHandler() { hk_state=0; hk_engaged=0; hk_down_tick=0; }
 	EventHandlerFn LastEventHandler() { 
 		ResetEventHandler();
@@ -52,12 +52,16 @@ public:
 	bool __cdecl SinglePressCtrlAltEventHandler(WPARAM wParam, KBDLLHOOKSTRUCT* kb_event) asm("_SinglePressCtrlAltEventHandler");
 	bool __cdecl SinglePressShiftAltEventHandler(WPARAM wParam, KBDLLHOOKSTRUCT* kb_event) asm("_SinglePressShiftAltEventHandler");
 	bool __cdecl SinglePressCtrlShiftEventHandler(WPARAM wParam, KBDLLHOOKSTRUCT* kb_event) asm("_SinglePressCtrlShiftEventHandler");
+	bool __cdecl LongPressCtrlAltEventHandler(WPARAM wParam, KBDLLHOOKSTRUCT* kb_event) asm("_LongPressCtrlAltEventHandler");
+	bool __cdecl LongPressShiftAltEventHandler(WPARAM wParam, KBDLLHOOKSTRUCT* kb_event) asm("_LongPressShiftAltEventHandler");
+	bool __cdecl LongPressCtrlShiftEventHandler(WPARAM wParam, KBDLLHOOKSTRUCT* kb_event) asm("_LongPressCtrlShiftEventHandler");
 #endif
 };
 
 wchar_t np[]=L"notepad.exe"; 
+wchar_t cl[]=L"calc.exe"; 
 
-KeyTriplet2 OnKeyTriplet2(np, NULL);
+KeyTriplet2 OnKeyTriplet2(np, cl);
 
 void HotkeyEventHandler(wchar_t* snk_cmdline_buf) {
 	STARTUPINFO si={sizeof(STARTUPINFO)};
@@ -124,12 +128,47 @@ void Test2()
 	OnKeyTriplet2.SinglePressCtrlAltEventHandler(WM_KEYUP, &kb_eventCTRL);
 }
 
+void Test3()
+{
+	KBDLLHOOKSTRUCT kb_eventBS={0x08, 0x0E};
+	KBDLLHOOKSTRUCT kb_eventBS2={0x08, 0x0E, 0x00, 3000};
+	KBDLLHOOKSTRUCT kb_eventCTRL={0xFFFFFFFF, 0x1D};
+	KBDLLHOOKSTRUCT kb_eventALT={0xFFFFFFFF, 0x38};
+	
+	OnKeyTriplet2.LongPressCtrlAltEventHandler(WM_KEYDOWN, &kb_eventCTRL);
+	OnKeyTriplet2.LongPressCtrlAltEventHandler(WM_KEYDOWN, &kb_eventALT);
+	OnKeyTriplet2.LongPressCtrlAltEventHandler(WM_KEYDOWN, &kb_eventBS);
+	OnKeyTriplet2.LongPressCtrlAltEventHandler(WM_KEYDOWN, &kb_eventBS);
+	OnKeyTriplet2.LongPressCtrlAltEventHandler(WM_KEYUP, &kb_eventBS2);
+	OnKeyTriplet2.LongPressCtrlAltEventHandler(WM_KEYUP, &kb_eventALT);
+	OnKeyTriplet2.LongPressCtrlAltEventHandler(WM_KEYUP, &kb_eventCTRL);
+}
+
+void Test4()
+{
+	KBDLLHOOKSTRUCT kb_eventBS={0x08, 0x0E};
+	KBDLLHOOKSTRUCT kb_eventBS2={0x08, 0x0E, 0x00, 1000};
+	KBDLLHOOKSTRUCT kb_eventCTRL={0xFFFFFFFF, 0x1D};
+	KBDLLHOOKSTRUCT kb_eventALT={0xFFFFFFFF, 0x38};
+	
+	OnKeyTriplet2.LongPressCtrlAltEventHandler(WM_KEYDOWN, &kb_eventCTRL);
+	OnKeyTriplet2.LongPressCtrlAltEventHandler(WM_KEYDOWN, &kb_eventALT);
+	OnKeyTriplet2.LongPressCtrlAltEventHandler(WM_KEYDOWN, &kb_eventBS);
+	OnKeyTriplet2.LongPressCtrlAltEventHandler(WM_KEYDOWN, &kb_eventBS);
+	OnKeyTriplet2.LongPressCtrlAltEventHandler(WM_KEYUP, &kb_eventBS2);
+	OnKeyTriplet2.LongPressCtrlAltEventHandler(WM_KEYUP, &kb_eventALT);
+	OnKeyTriplet2.LongPressCtrlAltEventHandler(WM_KEYUP, &kb_eventCTRL);
+}
+
 int main(int argc, char* argv[])
 {
 	if (argc<=1) {
 		std::wcout<<L"FUCK YOU"<<std::endl;
 		return 0;
 	}
+	
+	SetProcessAffinityMask(GetCurrentProcess(), (1<<0));
+	SetThreadPriority(GetCurrentThread(), THREAD_PRIORITY_TIME_CRITICAL);
 	
 	if (!strcmp(argv[1], "b")) {
 		std::wcout<<L"TEST B"<<std::endl;
@@ -143,27 +182,31 @@ int main(int argc, char* argv[])
 		lng=true;
 	} else if (!strcmp(argv[1], "e")) {
 		std::wcout<<L"TEST E"<<std::endl;
+		Test2();
+		return 0;
+	} else if (!strcmp(argv[1], "f")) {
+		std::wcout<<L"TEST F"<<std::endl;
+		Test3();
+		return 0;
+	} else if (!strcmp(argv[1], "g")) {
+		std::wcout<<L"TEST G"<<std::endl;
+		Test4();
+		return 0;
 	} else {
 		std::wcout<<L"TEST A"<<std::endl;
 	}
 	
 	std::wstring none;
 	
-	SetProcessAffinityMask(GetCurrentProcess(), (1<<0));
-	SetThreadPriority(GetCurrentThread(), THREAD_PRIORITY_TIME_CRITICAL);
-	
 	OnKeyTriplet.SetOnShortHotkey(std::bind(&HotkeyEventHandler, const_cast<wchar_t*>(none.c_str())));
 	OnKeyTriplet.SetOnLongHotkey(std::bind(&HotkeyEventHandler, const_cast<wchar_t*>(none.c_str())));
 	OnKeyTriplet.SetLongPress(true);
 	
-	if (strcmp(argv[1], "e")) {
-		for (int i=1; i<=1000; i++)
-			Test1(i);
-		
-		std::wcout<<L"RESULT = "<<cycles/1000<<std::endl;
-		std::cerr<<cycles/1000<<";";
-	} else
-		Test2();
+	for (int i=1; i<=1000; i++)
+		Test1(i);
+	
+	std::wcout<<L"RESULT = "<<cycles/1000<<std::endl;
+	std::cerr<<cycles/1000<<";";
 	
 	return 0;
 }
